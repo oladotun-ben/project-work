752521875e09a188d69cb79e81b7e059ec1d2a23
ben josiah <benjosiah90@gmail.com>
2021-07-09 15:49:14 +0100

    students info

--- StudentOne.js
+++ StudentOne.js
@@ -10,3 +10,59 @@ function showCheckboxes() {
     expanded = false;
   }
 }
+
+function showCheck() {
+  var checkbox = document.getElementById("checkbox");
+  if (!expanded) {
+    checkbox.style.display = "block";
+    expanded = true;
+  } else {
+    checkbox.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes3() {
+  var checkboxes3 = document.getElementById("checkboxes3");
+  if (!expanded) {
+    checkboxes3.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes3.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes4() {
+  var checkboxes4 = document.getElementById("checkboxes4");
+  if (!expanded) {
+    checkboxes4.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes4.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes5() {
+  var checkboxes5 = document.getElementById("checkboxes5");
+  if (!expanded) {
+    checkboxes5.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes5.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes6() {
+  var checkboxes6 = document.getElementById("checkboxes6");
+  if (!expanded) {
+    checkboxes6.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes6.style.display = "none";
+    expanded = false;
+  }
+  
+}
